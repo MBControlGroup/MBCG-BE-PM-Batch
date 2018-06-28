d6b7b19817bb19414f37dacc56e71361a4b1ace8
xujj25 <xujj25@foxmail.com>
2018-06-28 23:38:04 +0800

    增加跨域配置

--- main.go
+++ main.go
@@ -6,7 +6,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"routes"
+
+	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
 )
@@ -42,18 +45,42 @@ func init() {
 	listenAddr = fmt.Sprintf("%s:%s", globalCfg.ServerCfg.IPAddr, globalCfg.ServerCfg.Port)
 }
 
+// CorsHeadersMiddleWare 跨域头设置中间件
+func CorsHeadersMiddleWare() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		ctx.Next()
+	}
+}
+
 func main() {
 	defer func() {
 		dao.MyDB.Close()
 	}()
 
 	router := gin.Default()
+	router.Use(cors.Default())
+	router.Use(CorsHeadersMiddleWare()) // 允许所有源的cors配置
+
+	router.OPTIONS("/*action", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, struct{}{})
+	})
+
+	// test
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "ok")
+	})
 
 	// 批量导入
 	router.POST("/upload", routes.Upload)
 
 	// 批量导出
 	router.GET("/download", routes.Download)
+
+	router.POST("/try", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "try ok")
+	})
 
 	router.Run(listenAddr)
 }
